d29abf18e8fce6939e5327fc1856ca35dc581046
Luxray5474 <skybluehexapon@outlook.com>
2019-03-21 18:07:53 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dna2rna2aa
-Transcribes DNA into mRNA and the resulting Amino Acid chain.
+
+`dna2rna2aa <dna string>`
+
+A CLI application that transcribes DNA into mRNA and the resulting Amino Acid chain. Made to be lightweight, and do the heavy lifting of Biology assignments. You can clone this or [install it with NPM](https://www.npmjs.com/package/dna2rna2aa).
+
+If you just want to start using it, there is a **[fully working copy on Repl.it](https://repl.it/@ckurtdolittle/dna2rna2aa)**
---
+++ index.js
@@ -0,0 +1,51 @@
+var dna = process.argv[2].split("");
+var arg = process.argv[3];
+var mRNA = [], aa = [], aco = [];
+
+var transCribeLate = function(iBs, oBs, iBa, outArr) {
+  for(z=0;z<oBs.length;z++) if(iBs[z].includes(iBa)) outArr.push(oBs[z]);
+}
+
+if(dna.length % 3 == 0) {
+  var i, j, tempArr;
+
+  for(i=0;i<dna.length;i++) transCribeLate([["G","g"],["T","t"],["A","a"],["C","c"]], ["C","A","U","G"], dna[i], mRNA);
+
+  console.log("mRNA output:");
+  console.log(mRNA.join(""));
+
+  console.log("\nCodon, anti-codon and Amino Acid output:\n\ncod aco aa\n---+---+---------");
+  for(i=0,j=mRNA.length; i<j; i+=3) {
+    tempArr = mRNA.slice(i,i+3);
+
+    for(k=0;k<3;k++) transCribeLate([["G"],["U"],["A"],["C"]], ["C","A","U","G"], tempArr[k], aco);
+    
+    transCribeLate([
+      ["GCC","GCU","GCA","GCG"],//             Ala
+      ["AGA","AGG","CGA","CGG","CGU","CGC"],// Arg
+      ["GAC","GAU"],["AAC","AAU"],//           Asp, Asn
+      ["UGC","UGU"],["GAA","GAG"],//           Cys, Glu
+      ["CAA","CAG"],//                         Gln
+      ["GGA","GGG","GGC","GGU"],//             Gly
+      ["CAC","CAU"],["AUA","AUC","AUU"],//     His, Ile
+      ["UUA","UUG","CUA","CUG","CUU","CUC"],// Leu
+      ["AAA","AAG",],["AUG"],["UUC","UUU"],//  Lys, Met START, Phe
+      ["CCA","CCG","CCU","CCC"],//             Pro
+      ["AGC","AGU","UCA","UCG","UCC","UCU"],// Ser
+      ["ACA","ACG","ACU"],//                   Thr
+      ["UGG"],["UAC","UAU"],//                 Trp
+      ["GUA","GUG","GUC","GUU"],//             Tyr
+      ["UAG"],["UAA"],["UGA"]//                Amb STOP, Och STOP, Opa STOP
+    ], [
+      "Ala","Arg","Asp","Asn","Cys",
+      "Glu","Gln","Gly","His","Ile",
+      "Leu","Lys","Met START","Phe","Pro",
+      "Ser","Thr","Trp","Tyr","Val",
+      "Amb STOP", "Och STOP", "Opa STOP" 
+    ], tempArr.join(""), aa);
+    
+    console.log(`${tempArr.join("")} ${aco.join("")} ${aa.join("")}`);
+    aco=[];
+    aa=[];
+  }
+} else console.log("Input must be divisible by three!");
